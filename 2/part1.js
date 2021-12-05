0b7f2ae32b4a447d7a4c831e9f06255e6b43f637
Nico Rausch <rausch.nico@gmail.com>
2021-12-05 22:01:05 +0100

    Well that was unintended.

--- 2/part1.js
+++ 2/part1.js
@@ -1,4 +1,1 @@
-Math.imul(...document.body.innerText.split(/\n(?!$)/).reduce((a,c)=>([d,n]=c.split(" "),i=d[0]=='f'?1:0,m=d[0]=='u'?-1:1,a[i]+=m*n,a),[0,0]))
-
-// part 2
-Math.imul(...document.body.innerText.split(/\n(?!$)/).reduce((a,c)=>([d,n]=c.split(" "),i=d[0]=='f'?0:2,m=d[0]=='u'?-1:1,a[i]+=m*n,i||(a[1]+=a[2]*n),a),[0,0,0]))
+(k=document.body.innerText.split(/\n(?!$)/).reduce((a,c)=>([d,n]=c.split(" "),i=d[0]=='f'?1:0,m=d[0]=='u'?-1:1,a[i]+=m*n,a),[0,0]),k[0]*k[1])
